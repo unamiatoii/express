cab2b2778559f1e605d8b5c6a1a88a2e27e6fd74
unamiatoii <pierrejaphetkonan@gmail.com>
2024-05-28 15:54:31 +0000

    SOme changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// index.js
+// app.js
 
 const express = require('express');
 const app = express();
@@ -7,6 +7,29 @@ const port = 3000;
 // Route de base
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+// Route avec un paramètre dynamique
+app.get('/user/:id', (req, res) => {
+  const userId = req.params.id;
+  res.send(`Utilisateur ID : ${userId}`);
+});
+
+// Route avec une méthode POST
+app.post('/user', (req, res) => {
+  res.send('Requête POST reçue');
+});
+
+// Route avec une méthode PUT
+app.put('/user/:id', (req, res) => {
+  const userId = req.params.id;
+  res.send(`Utilisateur ID : ${userId} mis à jour`);
+});
+
+// Route avec une méthode DELETE
+app.delete('/user/:id', (req, res) => {
+  const userId = req.params.id;
+  res.send(`Utilisateur ID : ${userId} supprimé`);
 });
 
 // Démarrage du serveur
